36b6d5922af247c7e77795fbc1bbc38294165857
อากิ <akiiaaa631@gmail.com>
2024-10-13 15:44:00 +0700

    Create Fullscript.lua

---
+++ Fullscript.lua
@@ -0,0 +1,6 @@
+if game.PlaceId == 15939808257 then
+    print ("pixel")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/akaiira2547/script_pixel_td/refs/heads/main/pixel_tower_defend.lua"))()
+else 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/akaiira2547/script_pixel_td/refs/heads/main/macro_pixel.lua"))()
+end
